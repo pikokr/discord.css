35501f88d0a72be57ffe610b128f207f1d35b53c
파랑이 <parangee9706@gmail.com>
2020-10-27 01:44:49 +0000

    commit

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,18 @@
+function updateDark() {
+    if (!localStorage.getItem('dark') || localStorage.getItem('dark') === 'true') {
+        document.body.setAttribute('dark', '')
+        document.querySelector('#theme-switcher').innerHTML
+    }
+    else if (localStorage.getItem('dark') === 'false') document.body.removeAttribute('dark')
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-    
+    updateDark()
+
+    document.querySelector('#theme-switcher').addEventListener('click', () => {
+        const dark = localStorage.getItem('dark')
+        if (!dark || dark === 'true') localStorage.setItem('dark', 'false')
+        else localStorage.setItem('dark', 'true')
+        updateDark()
+    })
 })
